0dd8698a9533a3d72a218221ce5e6f25aec8b5ab
Carlos Miranda <carlos.gm@outlook.com.br>
2021-07-08 15:23:45 -0300

    aula

---
+++ 01-exercicio.rb
@@ -0,0 +1,5 @@
+puts "Em um ano há " + (24*365).to_s + " horas"
+puts "Em uma década há " + (60*24*365*10).to_s + " minutos"
+puts "Eu tenho " + (60*60*24*365*22).to_s + " segundos de idade"
+puts "Pretendo comer " + (3*12*100).to_s + " chocolates"
+puts "Se sua idade em segundos é 1232 milhões de segundos, em anos ela será " + (1232000000/60/60/24/365).to_s

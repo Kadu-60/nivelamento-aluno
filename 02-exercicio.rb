b3edd7e2178395ef10736be56cc72fb0c9ae8d49
Carlos Miranda <carlos.gm@outlook.com.br>
2021-07-08 17:19:12 -0300

    aula

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -10,9 +10,10 @@ minuto = 60
 hora = 60 
 dia = 24 
 ano = 365 
+idade = 22
 
 puts "Em um ano há " + (ano * dia).to_s + " horas"
 puts "Em uma década há " + (10 * ano * dia * hora).to_s + " minutos"
-puts "Eu tenho " + (22 * ano * dia * hora * minuto * segundo).to_s + " segundos de idade"
+puts "Eu tenho " + (idade * ano * dia * hora * minuto * segundo).to_s + " segundos de idade"
 puts "Pretendo comer " + (ano * 1 * 10).to_s + " chocolates"
 puts "Se sua idade em segundos é 1232 milhões de segundos, em anos ela será " + (1232000000/ano / dia / hora / minuto / segundo).to_s

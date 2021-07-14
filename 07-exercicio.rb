6999fe00f5282f2a3e4c8c92ae019a9bba6a7bc7
Kadu-60 <87090817+Kadu-60@users.noreply.github.com>
2021-07-14 18:09:19 -0300

    aula, refeito

--- 07-exercicio.rb
+++ 07-exercicio.rb
@@ -6,17 +6,22 @@
 # o programa deve imprimir na tela uma mensagem de que o chute é inválido.
 
 puts "Bem-vindo ao jogo de adivinhação!"
-puts "O computador pensou em um número entre 1 e 100. Tente adivinhar!"
+puts "O computador pensou em um número entre 0 e 100. Tente adivinhar!"
 
 # computador pensando no número...
-numero = rand(1..100)
+numero = rand(0..100)
 
 
-chute = gets.chomp.to_i
 
-if chute == 0 || chute > 100
+chute = gets.chomp
 
-    puts "ERRO, valor precisa ser um numero do intervalo de 1 a 100"
+verifica = chute.to_i
+
+
+
+if verifica.to_s != chute || chute.to_i < 0 || chute.to_i > 100
+
+    puts "ERRO, valor precisa ser um numero do intervalo de 0 a 100"
 
     elsif numero == chute
     puts "Acertou! O número é " + "(" + numero.to_s + ")"
